e656e9188607668c07471eee172cb5d2fa408a23
aritrikaTuteck <aritrika.goswami@tuteck.com>
2023-07-03 13:51:19 +0530

    code modified as per requirements, login, reset_password, forgotten_password completed

--- schemas/userDTO.py
+++ schemas/userDTO.py
@@ -1,18 +1,23 @@
 from pydantic import BaseModel,EmailStr,constr
 
 class UserIn(BaseModel):
-    id : int | None = None
+    #password_pattern = r"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$"
+
+    id : str | None = None
     first_name : str | None = None 
     last_name : str  | None = None
     email : EmailStr | None = None
     phone : constr(strip_whitespace=True) | None = None
-    password : constr(min_length=8, strip_whitespace=True) 
+    password : constr(min_length=8, strip_whitespace=True) #regex = password_pattern) 
     age : int | None = None
     gender : str | None = None
     address : constr(strip_whitespace=True) | None = None
 
     class Config:
         orm_mode = True
+
+
+        
 
 
     
